71d5b68afb05f37ea72cf835b2c48f0c40ecfec9
DobisPR <47193076+JMulligan87@users.noreply.github.com>
2019-11-22 23:49:14 -0500

    moved state to app.js

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import UserItem from './UserItem';
 
-export class Users extends Component {
+class Users extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div style={userStyle}>
+        {this.props.users.map(user => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
   }
 }
+
+const userStyle = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3,1fr)',
+  gridGap: '1rem'
+};
 
 export default Users;
